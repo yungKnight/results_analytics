fc661f06c1ee30b2b44e6478420848dfcb90e052
yungKnight <badoknight1@gmail.com>
2024-08-08 19:26:44 +0100

    Update to collector app
    This update fixes the template rendering issue with the former

--- eduNalytics/collector/views.py
+++ eduNalytics/collector/views.py
@@ -6,20 +6,18 @@ def scrape(request):
     if request.method == "POST":
         matric_number = request.POST.get("matric_number")
         password = request.POST.get("password")
-        
-        # Run the scrape function
+
         scrape_result = asyncio.run(run_scrape_script(matric_number, password))
 
-        # Prepare the data for the table
         course_details = []
         for course in scrape_result['CourseResults']:
             course_details.append({
                 'session': course.get('Session', 'unavailable'),
                 'semester': course.get('Semester', 'unavailable'),
                 'course_code': course.get('Course', 'unavailable'),
-                'branch': 'unavailable',  # Branch not yet registered in the database
+                'branch': 'unavailable',
                 'grade': course.get('Grade', 'unavailable'),
-                'unit': 'unavailable'  # Units not yet registered in the database
+                'unit': 'unavailable'
             })
 
         context = {
@@ -27,7 +25,6 @@ def scrape(request):
             'student_info': scrape_result['StudentInfo'],
         }
 
-        # Render the results in a new HTML page
-        return render(request, 'collector/assessment.html', context)
+        return render(request, 'assessment.html', context)
 
     return redirect('home:home')
