da31467b52ace2d5efa3039f7c3b61a2020e58fd
yungKnight <badoknight1@gmail.com>
2024-12-20 00:28:17 +0100

    This handles input validation for my username input on te landing page

--- eduNalytics/static/js/welcome.js
+++ eduNalytics/static/js/welcome.js
@@ -1,16 +1,14 @@
 const usernameInput = document.getElementById('username');
 const submitButton = document.getElementById('submit');
 
-usernameInput.addEventListener('input',() => {
-    const username = usernameInput.value;
+const regex = /^(\s)?[A-Za-z]+(\s+[A-Za-z]*\s*)?$/;
 
-    console.log('Username input value:', username);
+submitButton.addEventListener('click', (e) => {
+    const username = usernameInput.value.trim();
+    console.log(username)
 
-    if (regex.test(username)) {
-        console.log('Valid username!');
-    } else {
-        console.log('Invalid username!');
+    if (!regex.test(username)) {
+        e.preventDefault();
+        alert('Invalid username! Please enter a valid name using alphabets only.');
     }
 });
-
-const regex = /^[A-Za-z]+(\s+[A-Za-z]*\s*)?$/;
