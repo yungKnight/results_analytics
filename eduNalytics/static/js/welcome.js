6a2442e2946f40dd3c3f4bd4aa5c021d4932fec3
yungKnight <badoknight1@gmail.com>
2024-12-19 04:08:40 +0100

    This updates are to my frontend functionality

--- eduNalytics/static/js/welcome.js
+++ eduNalytics/static/js/welcome.js
@@ -1,1 +1,16 @@
-console.log("Welcome to edunalytica")
+const usernameInput = document.getElementById('username');
+const submitButton = document.getElementById('submit');
+
+usernameInput.addEventListener('input',() => {
+    const username = usernameInput.value;
+
+    console.log('Username input value:', username);
+
+    if (regex.test(username)) {
+        console.log('Valid username!');
+    } else {
+        console.log('Invalid username!');
+    }
+});
+
+const regex = /^[A-Za-z]+(\s+[A-Za-z]*\s*)?$/;
