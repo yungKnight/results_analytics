4aba8cbd595ad34ae4c6895138fa736e9ae019dc
yungKnight <badoknight1@gmail.com>
2025-01-20 09:17:18 +0100

    This sets up the basis of my script accessing needed elements already with urther updates to come

---
+++ eduNalytics/static/js/visualizer.js
@@ -0,0 +1,8 @@
+console.log('Needed Data:', neededData);
+console.log(('-' * 50))
+
+const viewBtn = document.getElementById('view_analysis');
+const advisory= document.getElementById('advisory');
+const studentSpecificPerformance = document.getElementsById('studentSpecificPerformance');
+const studentSemesterPerformance = document.getElementsById('studentSemesterPerformance');
+const MoreOfOrNo =document.getElementById('toDoMoreOrNot');
