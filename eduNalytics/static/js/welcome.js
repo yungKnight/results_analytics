9cb093af1bdc726f90e82f6969af089d90ffe588
yungKnight <badoknight1@gmail.com>
2025-03-06 16:11:09 +0100

    this update handles the scroll trigger and some section animations

--- eduNalytics/static/js/welcome.js
+++ eduNalytics/static/js/welcome.js
@@ -11,3 +11,28 @@ submitButton.addEventListener('click', (e) => {
         alert('Invalid username! Please enter a valid name using alphabets only.');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var cards = document.querySelectorAll('.step-card');
+
+    gsap.set(cards, { opacity: 0, y: 20, scale: 1.1 });
+
+    const observer = new IntersectionObserver(entries => {
+        let visibleCards = entries.filter(entry => entry.isIntersecting).map(entry => entry.target);
+
+        if (visibleCards.length) {
+            gsap.to(visibleCards, {
+                opacity: 1,
+                y: 0,
+                scale: 1.0,
+                duration: 0.5,
+                stagger: 0.4,
+                ease: 'power2.out'
+            });
+
+            visibleCards.forEach(card => observer.unobserve(card));
+        }
+    }, { threshold: 0.5 });
+
+    cards.forEach(card => observer.observe(card));
+});
