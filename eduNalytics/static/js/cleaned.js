7945dfd0f442757be618fc54e02c86d2dbb88e77
yungKnight <badoknight1@gmail.com>
2025-03-15 07:09:10 +0100

    This update handles the handwritten display for student name with further updates to come for the button and student info sections

--- eduNalytics/static/js/cleaned.js
+++ eduNalytics/static/js/cleaned.js
@@ -6,4 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href += 'insight/visual';
         });
     }
+
+    let studentNameContainer = document.getElementById('student-name');
+    if (studentNameContainer) {
+        let name = studentNameContainer.textContent.trim();
+        console.log(name)
+        studentNameContainer.textContent = "";
+
+        name.split("").forEach(letter => {
+            let span = document.createElement("span");
+            span.textContent = letter;
+            span.style.opacity = "0";
+            studentNameContainer.appendChild(span);
+        });
+
+        gsap.to("#student-name span", {
+            opacity: 1,
+            duration: 0.05,
+            stagger: 0.03,
+            ease: "power4.out"
+        });
+    }
 });
